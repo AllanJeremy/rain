91767c2b6c4ee2e00e59ebacc908842f2a24c713
Allan Jeremy <aj.dev254@gmail.com>
2016-12-25 08:38:02 +0300

    Function to generate text for due date

---
+++ _experimental_date_time.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<html lang="en" >
+
+<head>
+        <?php require_once("handlers/header_handler.php");?>
+
+        <title><?php echo MyHeaderHandler::GetPageTitle();?></title>
+
+        <!--Site metadata-->
+        <?php MyHeaderHandler::GetMetaData(); ?>
+        
+        <link  rel="stylesheet" type="text/css" href="stylesheets/compiled-materialize.css"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+<main>
+<div class="container">
+<h2 class="center">DATE TIME</h2>
+<?php
+    require_once("handlers/session_handler.php");
+    require_once("handlers/db_info.php");
+    require_once("handlers/date_handler.php");
+    $due_days = 1;
+    $due_hours = 1;
+    $due_minutes = 0;
+
+    $due_info = EsomoDate::GetDueText(array("days"=>$due_days,"hours"=>$due_hours,"minutes"=>$due_minutes));
+    echo " <h3>".$due_info["due_text"]."</h3>";
+    echo "<h6> Due days = $due_days</h6>";
+    echo "<h6> Due hours = $due_hours</h6>";
+    echo "<h6> Due minutes = $due_minutes</h6>";
+?>
+
+</div>
+</main>
+<body>
+
+    <script type="text/javascript" src="js/jquery-2.0.0.js"></script>
+    <script type="text/javascript" src="js/materialize.js"></script>
+    <script src="js/masonry.pkgd.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+
+    <script>
+    $(document).ready(function() {
+        $('select').material_select();
+
+        //Ensure labels don't overlap text fields
+        Materialize.updateTextFields();//doesn't work
+    });
+        
+    function hideSideNav() {
+        $(".mobile-button-collapse").sideNav('hide');
+        
+        //console.log('already open');
+    }
+
+
+    </script>
+    </body>
+</html>
--- handlers/date_handler.php
+++ handlers/date_handler.php
@@ -1,5 +1,80 @@
 <?php
-class EsomoDate
+interface EsomoDateFunctions
 {
+    public static function GetDueText($date_difference = array("days"=>0,"hours"=>0,"minutes"=>0));#returns the text that will be used for due dates
+    public static function GetOptimalDateTime($date_input);#returns the optimal date and time used to display the text in browser
+    public static function GetDateDiff($date_sent,$due_date); #returns the difference between the sent date and the due date
+
+}
+class EsomoDate implements EsomoDateFunctions
+{
+    //returns the text that will be used for due dates
+    public static function GetDueText($date_difference = array("days"=>0,"hours"=>0,"minutes"=>0))
+    {
+        $due_text = "";
+        $due_class = "";
+
+        if($date_difference["days"] < 0)
+        {
+            $due_text = "Late!";
+        }
+        elseif($date_difference["days"] == 0)
+        {
+            if($date_difference["hours"]>0)
+            {
+                $due_text = "Due in ".$date_difference["hours"]."h";
+                if($date_difference["minutes"]>0)
+                {
+                    $due_text.=" and ".$date_difference["minutes"]."min";
+                }
+                elseif($date_difference["minutes"]==0)
+                {
+                    $due_text = "Due in ".$date_difference["hours"]."h";
+                }
+                else
+                {
+                    $due_text = "Late!";
+                }
+            }
+            else
+            {
+                if($date_difference["minutes"]>0)
+                {
+                    $due_text="Due in  ".$date_difference["minutes"]."min";
+                }
+                elseif($date_difference["minutes"]==0)
+                {
+                    $due_text = "Due Today!";
+                }
+                else
+                {
+                    $due_text = "Late!";
+                }
+            }
+
+        }
+        elseif($date_difference["days"] == 1)
+        {
+            $due_text = "Due Tomorrow!";
+        }
+        else
+        {
+            $due_text = "Due in ".$date_difference["days"]." days";
+        }
+
+        return array("due_text"=>$due_text,"due_class"=>$due_class); 
+    }
+
+    //returns the optimal date and time used to display the text in browser
+    public static function GetOptimalDateTime($date_input)
+    {
+
+    }
+
+    //returns the difference between the sent date and the due date
+    public static function GetDateDiff($date_sent,$due_date)
+    {
+        date_diff();
+    } 
     
 }
