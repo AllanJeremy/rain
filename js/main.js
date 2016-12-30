098bd6fe218100c54fab374321f0e7a2335b8d3b
mwauramuchiri <gramwauu@gmail.com>
2016-12-30 08:25:48 +0300

    Active tab stays active even after document reloading
    I've used the url #hash to achieve this

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function(z) {
+$(function (z) {
     
     //init the dashboard application except on login page
     if (location.pathname.split('/').pop() != 'login.php') {
@@ -9,43 +9,86 @@ $(function(z) {
     
     $('.mobile-button-collapse').sideNav();
     
-/*
-    var $container = $('#masonry-grid');
-    // initialize
-    $container.masonry({
-      columnWidth: '.card-col',
-      itemSelector: '.col',
-    });
-*/
-    
-    
     /**************
     TABS SWITCH FUNCTIONALITIES
     ************/
     
-    //hide all tabs in <main> then remove class on the active bar
+    
+    /********** ON DOCUMENT LOAD EVENT **********/
+    
+    //hide all tabs in <main> then remove 'hide' class on the active bar
     $('main .main-tab').addClass('hide');
     $('main .active-bar').removeClass('hide');
     
-    //on click none-collapsible-headers side-nav panel actions
-    $('.side-nav a.collapsible-header').click(function() {
-        //$('.side-nav a:not(.collapsible-header)').parent().removeClass('active');
+    
+    //make the current #? bar active on page load
+    console.log(location.hash);
+    
+    var currTag = location.hash;//current hash on init
+    
+    if(!currTag || currTag === '#!name' || currTag === '#!') {//if currTag is undefined or has '#!name' or '#!' value, make it #classroom
+        currTag = '#classroom';
+        location.hash = currTag;
+    }
+    
+    var activates = $('.side-nav a:not(.collapsible-header)' + currTag).attr('data-activates');//get the 'data-activates' attr of the current hash
+
+    $('main .active-bar').addClass('hide');
+
+    $('main .main-tab').removeClass('active-bar');
+
+    $('main #' + activates).addClass('active-bar');
+
+    $('main .active-bar').removeClass('hide');
+
+    $('.side-nav a:not(.collapsible-header)').parent().removeClass('active');//clean out any active class
+    
+    $('.side-nav a:not(.collapsible-header)' + currTag).parent('li').addClass('active');// make active the tag that's similar to the location hash
+    $('.side-nav a:not(.collapsible-header)' + currTag).parent('li').parent().parent('.collapsible-body').parent().addClass('active');// make active the collapsible header of the tag that's similar to the location hash
+    $('.side-nav a:not(.collapsible-header)' + currTag).parent('li').parent().parent().parent().parent('.collapsible').find('.collapsible-header').addClass('active');// make active the collapsible header of the tag that's similar to the location hash
+    $('.side-nav a:not(.collapsible-header)' + currTag).parent('li').parent().parent('.collapsible-body').css('display','block');// Open the collapsible body
+
+    var pageTitle = $('.side-nav a:not(.collapsible-header)' + currTag).text();
+
+    $('a#pageTitle').text(pageTitle);
+
+    $(document).on('click', '.side-nav a', function (e) {
+        
+        //console.log('tag id is -> ' + $(this).attr('id'));
+        
+        var tag = $(this).attr('id');
+        
+        if(tag) {
+            
+            location.hash = tag;//add the id to the url as hash '#'
+        
+        }
+        
     });
     
     //on click none-collapsible-headers side-nav panel actions
-    $('.side-nav a:not(.collapsible-header)').click(function(e) {
+    $('.side-nav a.collapsible-header').click(function () {
+        //$('.side-nav a:not(.collapsible-header)').parent().removeClass('active');
+        console.log('Materialize css collapsible on.');
+    });
+    
+    
+    /********** ON CLICK EVENT **********/
+    
+    //on click none-collapsible-headers side-nav panel actions
+    $('.side-nav a:not(.collapsible-header)').click(function (e) {
         
         //console.log($(this).attr('data-activates'));
         
         var activates = $(this).attr('data-activates');
         
         //if it's not the name in the side nav that is clicked, switch tabs
-        if($(this).attr('id') != 'name' && $(this).attr('id') != undefined) {
+        if ($(this).attr('id') != 'name' && $(this).attr('id') != undefined) {
             
             e.preventDefault();
             
             $('.side-nav a:not(.collapsible-header)').parent().removeClass('active');
-            if($(this).parent().parent().parent().hasClass('collapsible-body') === true) {
+            if ($(this).parent().parent().parent().hasClass('collapsible-body') === true) {
                 //console.log('is a collapsible list');
                 
             } else {
