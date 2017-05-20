49e5ef8189ff769bd55c5035bd62774dc90d9d75
Allan Jeremy <aj.dev254@gmail.com>
2017-05-20 20:23:58 +0300

    Sendmail with tracker - The tracker theoretically should send emails once called now.
    - TODO (Remove the debug text for checking if an email is sent marked as TODO: [debug])

--- classes/mail_generator.php
+++ classes/mail_generator.php
@@ -9,7 +9,7 @@ class EsomoMailGenerator
     const ACCOUNTS_CONTACT_EMAIL = "account@brookhurst.com";#TODO: Change this to a valid email
     const NO_REPLY_EMAIL = "noreply@brookhurst.com";
 
-    /*acc_type, first_name, last_name, username, password*/
+    /*NEW ACCOUNT EMAILS*/
     //New account created
     private static  function NewAccountEmail($first_name,$last_name,$username,$acc_type,$email_to="",$cc="",$bcc="")
     {
@@ -17,12 +17,12 @@ class EsomoMailGenerator
         $today = EsomoDate::GetOptimalDateText($today);
 
         $subject = "Your E-Learning account has been successfully created";
-        $body = "<h3>Your E-Learning account has been successfully created</h3><p>Dear $first_name $last_name, your $acc_type account has been successfully been created.</p><p>To login to your account, use this information <br>username: $username <br>password: $password</p><p>Remember to change your password in the account section as soon as you login to ensure security of your account</p><b><b>Note: Do not share your password with anyone</b></p><br><p>If you believe that this account creation was a mistake, feel free to contact us at ".self::SUPERUSER_CONTACT_EMAIL." with the subject of the email being 'WRONG ACCOUNT CREATION'</p><p>Do not reply to this message. <i>This message was automatically generated on $today</i></p>";
+        $message = "<h3>Your E-Learning account has been successfully created</h3><p>Dear $first_name $last_name, your $acc_type account has been successfully been created.</p><p>To login to your account, use this information <br>username: $username <br>password: $password</p><p>Remember to change your password in the account section as soon as you login to ensure security of your account</p><b><b>Note: Do not share your password with anyone</b></p><br><p>If you believe that this account creation was a mistake, feel free to contact us at ".self::SUPERUSER_CONTACT_EMAIL." with the subject of the email being 'WRONG ACCOUNT CREATION'</p><p>Do not reply to this message. <i>This message was automatically generated on $today</i></p>";
 
         //Consider sending mail from within this function
         $mail_data = array(
             "subject"=>$subject,
-            "body"=>$body,
+            "message"=>$message,
             "to"=>$email_to,
             "from"=>self::NO_REPLY_EMAIL,
             "cc"=>$cc,
--- classes/tracker.php
+++ classes/tracker.php
@@ -5,7 +5,8 @@ require_once(realpath(dirname(__FILE__) ."/../handlers/date_handler.php"));
 /*This class controls tracking of the system's performance and minimal feedback*/
 class EsomoTracker
 {
-    const EMAIL_ADDRESSES = "aj.dev254@gmail.com,gramwauu@gmail.com";
+    const DEV_EMAILS = "aj.dev254@gmail.com";
+    const FROM_EMAIL = "aj.cgeek@gmail.com";#TODO ~ Change this email
 
     public static $ip;
     public static $date;
@@ -25,6 +26,8 @@ class EsomoTracker
         self::$db_host = DB_HOST;
         self::$db_name = DB_NAME;
         self::$db_username = DB_USERNAME;
+        
+        $subject = "New RAIN E-Learning system Installation";
 
         $superuser_create_message = "";
         if($superuser_create_status)
@@ -36,18 +39,68 @@ class EsomoTracker
             $superuser_create_message = "Failed to created the superuser account.<br><b>Call count :</b> ".self::$call_count;
         }
 
+        $to = self::DEV_EMAILS;
+        $message = $superuser_create_message;
+        $message .= "<br><h3>Additional Install information</h3>";
+        $message .= "<p><b>IP Address : ".self::$ip."</p>";
+        $message .= "<p><b>Date Installed : ".self::$date."</p>";
+        $message .= "<p><b>Database host : ".self::$db_host."</p>";
+        $message .= "<p><b>Database name : ".self::$db_name."</p>";
+        $message .= "<p><b>Database username : ".self::$db_username."</p>";
+
+        $send_status = null;
         //Send this details to our emails
         try
-        {
+        {         
             //Try sending the details of the installation location to our emails
+            //Headers for html email
+            $headers[] = 'MIME-Version: 1.0';
+            $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+            // Additional headers
+            $headers[] = 'To: '.self::DEV_EMAILS;
+            $headers[] = 'From: '.self::FROM_EMAIL;
+            $headers[] = 'Cc: '.self::FROM_EMAIL;
+
+            $send_status = mail($to, $subject, $message, implode("\r\n", $headers));
+
+            if($send_status)
+            {
+                echo "<p class='grey-text'>Sent html mail</p>";#TODO [debug] ~ remove
+            }
+            else
+            {
+                echo "<p class='grey-text'>Failed to send html mail</p>";#TODO [debug] ~ remove
+            }
+            
         }
         catch(Exception $e)
         {
+            $headers = 'From: '.self::FROM_EMAIL . "\r\n" .
+                'Reply-To: '.self::DEV_EMAILS . "\r\n" .
+                'X-Mailer: PHP/' . phpversion();
             //Send the error message that was encountered to the stated emails
+            $send_status = mail($to, $subject, $superuser_create_message,$headers);
+
+            if($send_status)
+            {
+                echo "<p class='grey-text'>Sent normal mail</p>";#TODO [debug] ~ remove
+            }
+            else
+            {
+                echo "<p class='grey-text'>Failed to normal mail</p>";#TODO [debug] ~ remove
+            }
         }
         finally
         {
+            $headers = 'From: '.self::FROM_EMAIL . "\r\n" .
+                'Reply-To: '.self::DEV_EMAILS . "\r\n" .
+                'X-Mailer: PHP/' . phpversion();
+
             //Send log stating that the message sending was completed
+            mail($to,$subject,"Installation tracking complete",$headers);
+
+            
         }
     }
 }
