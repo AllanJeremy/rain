590116a8114200c7cdaca6f6170fe349082ca393
Allan Jeremy <aj.dev254@gmail.com>
2017-05-22 16:56:56 +0300

    Report problem page - Created the UI for the report page - Created  an email generator that generates email based on the data provided - Added validation for required input fields - Added ajax request and ajax handler for the request in db_handler - TODO: Add code to send the email using phpmailer (Gabriel)

--- classes/mail_generator.php
+++ classes/mail_generator.php
@@ -6,8 +6,10 @@ require_once(realpath(dirname(__FILE__) . "/../handlers/date_handler.php"));
 class EsomoMailGenerator
 {
     /*Constants*/
-    const ACCOUNTS_CONTACT_EMAIL = "account@brookhurst.com";#TODO: Change this to a valid email
-    const NO_REPLY_EMAIL = "noreply@brookhurst.com";
+
+    const DEV_EMAILS = "aj.dev254@gmail.com,gramwauu@gmail.com";
+    const ACCOUNTS_CONTACT_EMAIL = "account@rain.co.ke";#TODO: Change this to a valid email
+    const NO_REPLY_EMAIL = "noreply@rain.co.ke";
 
     /*NEW ACCOUNT EMAILS*/
     //New account created
@@ -58,5 +60,54 @@ class EsomoMailGenerator
     {
         $mail_generated = self::NewAccountEmail($first_name,$last_name,$username,"student",$email_to,$cc,$bcc);
         return $mail_generated;
+    }
+
+    //Report problem email
+    public static function ReportProblemEmail($data,$user_info)
+    {
+        $today = EsomoDate::GetCurrentDate();
+        $today = EsomoDate::GetOptimalDateText($today);
+        
+        #Sanitize all inputs to prevent XSS attacks (htmlspecialchars)
+        //Section 
+        $report_section = htmlspecialchars($data["report_section"]);
+
+        //Specific
+        $specific_section = "[Not provided]";
+        if(!empty($data["report_specific"]))
+        {
+            $specific_section = htmlspecialchars($data["report_specific"]);
+        }
+
+        //Details of the problem
+        $problem_details = "[Not provided]";
+        if(!empty($data["report_message"]))
+        {
+            $problem_details = htmlspecialchars($data["report_message"]);
+        }
+
+        $subject = "Rain E-Learning | Problem report";
+        $message = "<h4>Problem reported by ".$user_info["full_name"]."(Account type : ".$user_info["account_type"].") on ".$today."</h4>";
+        $message .= "<p><b>Section : </b>".$report_section."</p>";
+        $message .= "<p><b>Specifically : </b>".$specific_section."</p>";
+        $message .= "<p><b>Details </b><br>".ucfirst($problem_details)."<p>";
+
+        $non_html_message = "\nProblem reported by ".$user_info["full_name"]." on ".$today."";
+        $non_html_message .= "\nSection : ".$report_section;
+        $non_html_message .= "\nSpecifically : ".$specific_section;
+        $non_html_message .= "\nDetails \n".ucfirst($problem_details);
+
+        $email_data = array(
+            "from"=>self::DEV_EMAILS,
+            "to"=>self::DEV_EMAILS,
+            "subject"=>$subject,
+            "message"=>$message,
+            "alt_message"=>$non_html_message,
+            "cc"=>self::DEV_EMAILS,
+            "bcc"=>self::DEV_EMAILS
+        );
+
+        #Return the email data ~ can be used by phpmailer
+        return $email_data;
     }
 }
---
+++ report.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+
+<html lang="en" >
+    <head>
+        <?php require_once("handlers/header_handler.php");?>
+
+        <title>Report a problem | RAIN E-Learning</title>
+
+        <!--Site metadata-->
+        <?php MyHeaderHandler::GetMetaData(); ?>
+        
+        <link  rel="stylesheet" type="text/css" href="stylesheets/compiled-materialize.css"/>
+        <link  rel="stylesheet" type="text/css" href="stylesheets/pace-theme-flash.css"/>
+<!--        <script src="js/head.min.js"></script>-->
+        <script>
+//            head.load('https://fonts.googleapis.com/icon?family=Material+Icons');
+        </script>
+
+        
+    </head>
+
+    <body class="grey lighten-5">
+
+        <?php 
+            require_once("handlers/session_handler.php");
+            require_once("handlers/global_init_handler.php");
+        ?>
+        <main>
+        <?php
+            //Only display the contents of the page if there is a user logged in
+            if(MySessionHandler::AdminIsLoggedIn() || MySessionHandler::StudentIsLoggedIn()):
+        ?>
+            <br>
+        <div class="container">
+            <div class="card-panel grey lighten-3">
+                <h5 class="center grey-text text-darken-2">Hi. You can report any problems you found here</h5>
+                <p class="center grey-text">
+                     If you found a problem, we're looking forward to hearing about it from you. Your feedback goes a long way in helping us create a better system for you.
+                </p>
+            </div>
+
+            <div class="card">
+                <div class="card-content row">
+                    <span class="card-title">Report a problem</span>
+                    <div class="divider"></div><br>
+                    <div class="col s12 m6 input-field" title="[Required] Specific section where the problem occurred">
+                        <select id="report_section" required>
+                            <option value="Classrooms">Classrooms</option>
+                            <option value="Assignments">Assignments</option>
+                            <option value="Schedules">Schedules</option>
+                            <option value="Tests">Tests</option>
+                            <option value="Resources">Resources</option>
+                            <option value="Account">Account</option>
+                            <option value="Other">Other</option>
+                        </select>
+                        <label for="report_section">Section problem occurred <sup>*</sup></label>
+                    </div>
+                    <div class="col s12 m6 input-field" title="[Optional] Specific section/subsection the problem occurred">
+                        <input type="text" placeholder="Specifically..." id="report_specific">
+                        <label for="report_specific">Specifically</label>
+                    </div>
+
+                    <div class="col s12 input-field" title="[Required] The details of the problem or any feedback you have regarding the problem">
+                        <textarea class="materialize-textarea" required placeholder="Details on the problem you found" id="report_message"></textarea>
+                        <label for="report_message">Problem details &amp feedback <sup>*</sup></label>
+                    </div>
+                    <div class="divider"></div>
+                    
+                    <div class="col s12 m6">
+                        <small class="grey-text">Fields marked with * are <b>required fields</b> and must be filled before submitting the problem</small>
+                    </div>
+                    
+                    <div class="col s12 m6">
+                        <a href="javascript:void(0)" class="btn right" id="btn_send_problem_form">SEND</a>
+                    </div>
+                </div>
+
+                <div class="card-action grey lighten-2">
+                    <p class="center">If you believe you are here by accident. You can navigate <a href="index.php" class="blue-text">back to home</a></p>
+                </div>
+            </div>
+        </div>
+        <?php
+            else:#No users logged in ~ redirect to the login page
+        ?>
+            <script>window.location = "login.php"</script>
+        <?php
+            endif;
+        ?>
+        </main>
+
+        
+        <script type="text/javascript" src="js/jquery-2.0.0.js"></script>
+        <script type="text/javascript" src="js/materialize.js"></script>
+        <script type="text/javascript" src="js/moment.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+                /*Initialize select*/
+                $('select').material_select();
+
+                /*Initialize inputs*/
+
+
+                //Client side : Verify required fields inputs
+                function ValidateRequiredFields()
+                {
+                    var $inputs = $("input,textarea,select");
+                    var $cur_input = null;
+                    var cur_val = "";
+                    var is_valid = true;
+
+                    //Foreach input
+                    $inputs.each(function(){
+                        $cur_input = $(this);
+                        
+                        //If the item is required ~ check if the value
+                        if($cur_input.prop("required"))
+                        {
+                            //Reset validity
+                            $cur_input.removeClass("valid");
+                            $cur_input.removeClass("invalid");
+
+                            //Current value
+                            cur_val = $cur_input.val();
+                            
+                            //If the value is set ~ it is valid
+                            if(cur_val && cur_val!="")
+                            {
+                                $cur_input.addClass("valid");
+                            }
+                            else //Value is not set ~ required field is empty, invalid
+                            {
+                                $cur_input.addClass("invalid");
+                                is_valid = false;
+                            }
+                            console.log($cur_input);
+                            console.log("Current value = ",cur_val);
+                        }
+                    });
+
+                    return is_valid;
+                }
+                /*Send button clicked*/
+                $("#btn_send_problem_form").click(function(){
+                    //Check that the required fields have been filled
+                    var valid_input = ValidateRequiredFields();
+
+                    //If the required inputs are valid/set ~ submit the form
+                    if(valid_input)
+                    {
+                        var data = {
+                            "report_section":$("#report_section").val(),
+                            "report_specific":$("#report_specific").val(),
+                            "report_message":$("#report_message").text()
+                        };
+
+                        var SUPPORT_EMAIL = "support@rain.co.ke";//TODO: Change or acquire this email
+
+                        //AJAX request
+                        $.post("handlers/db_handler.php",{"action":"ReportProblem","data":data},function(response,status){
+                            if(response == "1" || response=="true")
+                            {
+                                Materialize.toast("Successfully reported the problem. Thanks for your feedback",2500);
+                            }
+                            else
+                            {
+                                Materialize.toast("Failed to report the problem. Server side error: If the problem persists, contact support ("+SUPPORT_EMAIL+")",2500);
+                            }
+                        });
+                    }
+                    else
+                    {
+                        Materialize.toast("Failed to report problem. Reason: One or more required fields is empty.",2500);
+                    }
+                });
+            });
+        </script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </body>
+</html>
